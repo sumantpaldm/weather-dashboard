0c6a0a09ceb4a7b873c5402a7534a4e061ae48c0
SINGH 1258 <sumantpaldm@gmail.com>
2021-08-01 18:40:41 -0400

    updated js with date function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,32 @@
+//dom element refs
 var searchBtnEl = document.getElementById("searchBtn")
 var searchCityEl = document.getElementById("searchCity")
+// global vars url and empty array for history
 
 
+//time plugins  day.js
+let appDate = function (time) {
+    let displayDate = new Date();
+    displayDate.setTime(time * 1000);
+    let dd = displayDate.getDate();
+    let mm = displayDate.getMonth() + 1;
+    let y = displayDate.getFullYear();
+    return mm + '/' + dd + '/' + y;
+}
+
+
+//function to display search history list
+//start at end of history array
+
+
+
+
+//update the history in local storage and then it updates the history in display
+
+
+
+
+// function to set the item the again
 
 
 
@@ -15,7 +40,7 @@ var formSubmitHandler = function (event) {
 
     if (cityName) {
 
-        getData(cityName);
+        getInitialData(cityName);
         searchCityEl.value = "";
     } else {
         alert("Please enter a valid city !!!");
@@ -45,6 +70,9 @@ var getInitialData = function (cityName) {
 };
 
 var getMainData = function () {
+
+    let secondApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=ed2f4c47503611561c238cb4725495c3`;
+
 
 }
 
