9309fdc5f73151cac06309a9de5a6f78e01a0de7
SINGH 1258 <sumantpaldm@gmail.com>
2021-08-02 04:27:02 -0400

    final updates to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 //dom element refs
 var searchBtnEl = document.getElementById("searchBtn")
 var searchCityEl = document.getElementById("searchCity")
-// global vars url and empty array for history
+var boldDataEl = document.getElementById("boldData")
+var tempMainEl = document.getElementById("tempMain")
+var windMainEl = document.getElementById("windMain")
+var humidMainEl = document.getElementById("humidMain")
+var uvMainEl = document.getElementById("uvMain")
+var forcastEl = document.getElementById("forcast")
+let forcastBlock = document.createElement("div")
+let colorBlock = document.querySelector(".color")
 
-
-//time plugins  day.js
+searchHistoryBtn();
 let appDate = function (time) {
     let displayDate = new Date();
     displayDate.setTime(time * 1000);
@@ -13,21 +19,6 @@ let appDate = function (time) {
     let y = displayDate.getFullYear();
     return mm + '/' + dd + '/' + y;
 }
-
-
-//function to display search history list
-//start at end of history array
-
-
-
-
-//update the history in local storage and then it updates the history in display
-
-
-
-
-// function to set the item the again
-
 
 
 var formSubmitHandler = function (event) {
@@ -42,6 +33,7 @@ var formSubmitHandler = function (event) {
 
         getInitialData(cityName);
         searchCityEl.value = "";
+        history(cityName);
     } else {
         alert("Please enter a valid city !!!");
     }
@@ -69,31 +61,91 @@ var getInitialData = function (cityName) {
     });
 };
 
-var getMainData = function () {
-
-    let secondApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=ed2f4c47503611561c238cb4725495c3`;
+var getMainData = function (lat, log, cityName) {
 
 
+    let secondApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${log}&appid=ed2f4c47503611561c238cb4725495c3`;
+
+    fetch(secondApi)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+
+            boldDataEl.innerHTML = `<span class = "bold">${cityName}    (${appDate(data.current.dt)})  </span>   <img src="https://openweathermap.org/img/w/${data.current.weather[0].icon}.png" /> `
+            tempMainEl.innerHTML = `Temp: ${data.current.temp} &#186F`
+            windMainEl.innerHTML = `Wind: ${data.current.wind_speed} MPH`
+            humidMainEl.innerHTML = `Humidity: ${data.current.humidity} %`
+            uvMainEl.innerHTML = `UV Index: <span class = "color">      ${data.current.uvi}     </span>`
+            $(".main").addClass("mainDisplay")
+
+
+            if (data.current.uvi <= 2) {
+                $(".color").addClass("green");
+                $(".color").removeClass("yellow");
+                $(".color").removeClass("red");
+            } else if (data.current.uvi <= 5) {
+                colorBlock.classList.add("yellow");
+                colorBlock.classList.remove("green");
+                colorBlock.classList.remove("red");
+            } else {
+                colorBlock.classList.add("red");
+                colorBlock.classList.remove("yellow");
+                colorBlock.classList.remove("green");
+            }
+
+            $(forcastBlock).empty()
+
+            for (let i = 1; i < 6; i++) {
+                let forcastBlock = document.createElement("div")
+                forcastBlock.classList.add("fBlock")
+                forcastBlock.classList.add("col")
+
+                $(forcastBlock).append((data.daily[i].dt));
+                $(forcastBlock).append(`<img src="https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png"/>`);
+                $(forcastBlock).append("<p>Temp: " + data.daily[i].temp.day + " °F</p>");
+                $(forcastBlock).append("<p>Wind: " + data.daily[i].wind_speed + " MPH</p>");
+                $(forcastBlock).append("<p>Humidity: " + data.daily[i].humidity + " %</p>");
+
+                $(forcastEl).append(forcastBlock)
+            }
+        });
 }
 
 
 
+function history(cityName) {
+    $("#historyButtons").empty();
+    let savedCity = localStorage.getItem("cities")
+    let cityConditional = savedCity ? JSON.parse(savedCity) : [];
+    let currentCity = { cities: cityName }
 
+    cityConditional.push(currentCity);
 
+    localStorage.setItem("cities", JSON.stringify(cityConditional))
 
+    searchHistoryBtn()
+}
 
+function searchHistoryBtn() {
+    let cityList = localStorage.getItem("cities")
+    listConditional = cityList ? JSON.parse(cityList) : []
 
+    console.log("listConditional")
 
+    for (let i = 0; i < listConditional.length; i++) {
 
+        let historyBtn = document.createElement("button")
+        historyBtn.setAttribute("data", (listConditional[i].cities))
+        historyBtn.classList.add("buttons")
+        historyBtn.textContent = (listConditional[i].cities)
+        $("#historyButtons").append(historyBtn);
+    }
+}
 
-
-
-
-
-
-
-
-
+$("#historyButtons").on("click", function (event) {
+    getInitialData(event.target.textContent)
+})
 
 
 
